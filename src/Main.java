5f67574b69008e5e80e17883ff8fece4ca41ba4b
Mayur <mayurharisinghani@gmail.com>
2025-02-10 18:03:26 -0700

    Task 3 Complete

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner in = new Scanner(System.in);
+        String menuChoice = "";
+        System.out.println("Please choose your party affiliation [D, R or I]");
+        menuChoice = in.nextLine();
+        if (menuChoice.equalsIgnoreCase("D"))
+        {
+            System.out.println("You get a Democratic Donkey");
+        }
+        else if (menuChoice.equalsIgnoreCase("R"))
+        {
+            System.out.println("You get a Republican Elephant");
+        }
+        else if (menuChoice.equalsIgnoreCase("I"))
+        {
+            System.out.println("You get an Independent Man");
+        }
+        else
+            System.out.println("You chose Other");
     }
 }
